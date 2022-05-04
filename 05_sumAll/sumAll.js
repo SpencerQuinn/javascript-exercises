a910ab48f8bc6cc4949fcdde63e51245e85b6f47
Spencer Laboda <spence@Spencers-MacBook-Air.local>
2022-05-03 21:54:52 -0500

    Javascript exercises 3 - 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,11 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(typeof (num1+num2) !== 'number' || (num1 || num2) < 0) return "ERROR"
+    let range = [num1, num2].sort()
+    console.log(range)
+    let start = range[0],
+    end = range[1];
+    return ((end*(end+1))/2) - ((start*(start - 1))/2)
+
 
 };
 
